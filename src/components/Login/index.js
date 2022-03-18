ac75118d71d8a4be4fd54c6e082907f481980e9a
seo722 <95078290+seo722@users.noreply.github.com>
2022-03-18 23:00:36 +0900

    first commit

---
+++ src/components/Login/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { BgImage, Container, Content } from "./styles";
+
+const Login = () => {
+  return (
+    <Container>
+      <Content>
+        <BgImage />
+      </Content>
+    </Container>
+  );
+};
+
+export default Login;
